eaac0ff392762d4d5d0c47ba004ad1e78ce95476
Javas Shankar <javasshankar@gmail.com>
2025-11-20 23:52:53 -0500

    Updates

--- _next/static/rypVRTFoYub2u_peSTHs6/_buildManifest.js
+++ _next/static/rypVRTFoYub2u_peSTHs6/_buildManifest.js
@@ -1,9 +1,9 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/676831e4ef1da916.js"
+    "static/chunks/b35ea28d8a9200fb.js"
   ],
   "/_error": [
-    "static/chunks/05c3c806b66b2103.js"
+    "static/chunks/cbb1c9daa609b128.js"
   ],
   "__rewrites": {
     "afterFiles": [],
